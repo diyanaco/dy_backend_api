debedd89d9d123a64b1b05399a25d160b2e22f35
muhammadzaim <zaimsaharudin@gmail.com>
2022-07-02 22:00:56 +0800

    change env to prod

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from backend import app
 
 if __name__ == "__main__":
-    #app.run(host='0.0.0.0', debug=True)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+    #app.run(debug=True)

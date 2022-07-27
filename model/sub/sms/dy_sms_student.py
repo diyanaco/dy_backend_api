41fd2ec7ef104a0f2942772fa77994aa7f35733c
miazocool <zaimsaharudin@gmail.com>
2022-07-27 21:34:58 +0800

    Add col level

--- model/sub/sms/dy_sms_student.py
+++ model/sub/sms/dy_sms_student.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String, Table, DateTime
 from sqlalchemy.orm import relationship
 from model.sub.sms.dy_sms_student_class_link import  association_student_class_table
 from backend import Base
-
+#TODO #41 Update student model according to dbSchema
 class StudentModel(Base):
     __tablename__="dy_sms_student"
     id = Column(String(50), primary_key=True)
